3b150abbd6d7ff205331235be792204b02b84052
Jane Venter <janeanneventer@gmail.com>
2019-10-10 18:25:52 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // Code your solutions in this file
+const cards = ["Ada", "Brendan", "Ali"];
+function writeCards(cards, birthday) {
+  const newcards = [];
+  for(let i = 0; i < cards.length; i++) {
+    newcards.push(`Thank you, ${cards[i]}, for the wonderful ${birthday} gift!`);
+  }
+  return newcards;
+}
+
+writeCards(cards, birthday);
